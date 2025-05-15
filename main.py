3eb3b83f8f9ae3da60dbf209aed5e1891e9055ed
brunosag <brunosag02@gmail.com>
2025-05-15 17:07:17 -0300

    add async and word audio generation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import asyncio
+import inspect
 import os
+import sys
 
 from dotenv import load_dotenv
 from elevenlabs import play, save
-from elevenlabs.client import ElevenLabs
+from elevenlabs.client import AsyncElevenLabs
 from openai import OpenAI
 
 from anki.collection import Collection
@@ -30,20 +33,18 @@ def normalize_cards(col: Collection):
 
 
 def generate_sentence_and_explanation(col: Collection, note: Note):
-    word = note["word"]
-
     client = OpenAI()
     prompt = (
         "You are an assistant for beginner French learners.\n\n"
         "Given a French word:\n\n"
-        "1. Provide a short, simple French example sentence using it.\n"
-        "2. Provide a brief English explanation of the word's core meaning (no direct translation). Avoid repeating the word itself and skip introductions like 'The word means...'.\n"
+        "1. Write a short, simple French example sentence that clearly includes this exact word.\n"
+        "2. Give a brief English explanation of its meaning (not a direct translation). Avoid repeating the word and do not start with phrases like 'It means...' or 'This is a word describing...', give the meaning straight away.\n"
         "3. Separate the sentence and explanation with a '$'. Output nothing else.\n\n"
-        f"Word: {word}"
+        f"Word: {note["word"]}"
     )
-    print(f"Generating sentence and explanation for '{word}'...")
+
+    print("\tGenerating sentence and explanation...")
     response = client.responses.create(model="gpt-4.1", input=prompt)
-    print("Done.")
 
     [sentence, explanation] = response.output_text.split("$")
     note["sentence"] = sentence
@@ -52,58 +53,92 @@ def generate_sentence_and_explanation(col: Collection, note: Note):
     col.update_note(note)
 
 
-def generate_audios(col: Collection, note: Note):
-    sentence = note["sentence"]
-    explanation = note["explanation"]
-
-    client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
+async def generate_audios(col: Collection, note: Note):
+    client = AsyncElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
     voice_id = "ohItIVrXTBI80RrUECOD"
 
-    print(f"Generating sentence audio for '{note["word"]}'...")
-    sentence_audio = client.text_to_speech.convert(
-        text=sentence,
-        voice_id=voice_id,
-    )
-    print("Done.")
-    print(f"Generating explanation audio for '{note["word"]}'...")
-    explanation_audio = client.text_to_speech.convert(
-        text=explanation,
-        voice_id=voice_id,
-    )
-    print("Done.")
+    if not note["word_audio"].startswith("["):
+        print("\tGenerating word audio...")
+        result = client.text_to_speech.convert(
+            model_id="eleven_turbo_v2_5",
+            voice_id=voice_id,
+            language_code="fr",
+            text=note["word"],
+        )
+        audio_chunks = [chunk async for chunk in result]
+        word_audio = b"".join(audio_chunks)
+        save(
+            word_audio,
+            os.path.join(col.media.dir(), f"{note.id}_word.mp3"),
+        )
+        note["word_audio"] = f"[sound:{note.id}_word.mp3]"
 
-    sentence_audio_filename = "sentence_audio.mp3"
-    explanation_audio_filename = "explanation_audio.mp3"
+    if not note["sentence_audio"]:
+        print("\tGenerating sentence audio...")
+        result = client.text_to_speech.convert(
+            model_id="eleven_turbo_v2_5",
+            voice_id=voice_id,
+            language_code="fr",
+            text=note["sentence"],
+        )
+        audio_chunks = [chunk async for chunk in result]
+        sentence_audio = b"".join(audio_chunks)
+        save(sentence_audio, os.path.join(col.media.dir(), f"{note.id}_sentence.mp3"))
+        note["sentence_audio"] = f"[sound:{note.id}_sentence.mp3]"
 
-    save(sentence_audio, os.path.join(col.media.dir(), f"{note.id}_sentence.mp3"))
-    save(explanation_audio, os.path.join(col.media.dir(), f"{note.id}_explanation.mp3"))
-
-    note["sentence_audio"] = f"[sound:{note.id}_sentence.mp3]"
-    note["explanation_audio"] = f"[sound:{note.id}_explanation.mp3]"
+    if not note["explanation_audio"]:
+        print("\tGenerating explanation audio...")
+        result = client.text_to_speech.convert(
+            model_id="eleven_turbo_v2_5",
+            voice_id=voice_id,
+            language_code="en",
+            text=note["explanation"],
+        )
+        audio_chunks = [chunk async for chunk in result]
+        explanation_audio = b"".join(audio_chunks)
+        save(
+            explanation_audio,
+            os.path.join(col.media.dir(), f"{note.id}_explanation.mp3"),
+        )
+        note["explanation_audio"] = f"[sound:{note.id}_explanation.mp3]"
 
     col.update_note(note)
 
 
-def fill_n_cards(col: Collection, n: int):
+async def fill_n_cards(col: Collection, n: int):
     note_ids = col.find_notes("deck:essential_french")
     count = 0
+
     for note_id in note_ids:
         note = col.get_note(note_id)
         generated = False
-        if not note["sentence"] or not note["explanation"]:
+        if not (note["sentence"] and note["explanation"]):
+            print(note_id)
             generate_sentence_and_explanation(col, note)
             generated = True
-        if not note["sentence_audio"] or not note["explanation_audio"]:
-            generate_audios(col, note)
+        if not (
+            note["sentence_audio"]
+            and note["explanation_audio"]
+            and note["word_audio"].startswith("[")
+        ):
+            if not generated:
+                print(note_id)
+            await generate_audios(col, note)
             generated = True
         if generated:
             count += 1
-        if count == 10:
+        if count == n:
             break
 
 
-col = Collection(COL_PATH)
+async def main():
+    n = int(sys.argv[1])
+    col = Collection(COL_PATH)
+    try:
+        await fill_n_cards(col, n)
+    finally:
+        col.close()
 
-fill_n_cards(col, 10)
 
-col.close()
+if __name__ == "__main__":
+    asyncio.run(main())
